c2179b77abb011ebb51f5b00bd236da1930de7b3
Sergei <lyamin.sls@gmail.com>
2025-01-31 22:28:37 +0500

    Structuring Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,1 +1,20 @@
-<h1></h1>
+// import React from 'react';
+import {Logo} from "../../components/logo/Logo.tsx";
+import styled from "styled-components";
+import {Menu} from "../../components/menu/Menu.tsx";
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Logo/>
+            <Menu/>
+        </StyledHeader>
+    );
+};
+
+const StyledHeader = styled.header`
+    background-color: aqua;
+    display: flex;
+    justify-content: space-between;
+`
+
