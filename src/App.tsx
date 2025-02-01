1d2c32b384f0e2dd6bf2b90e59d8f9c3cf8f6434
Sergei <lyamin.sls@gmail.com>
2025-02-01 18:26:52 +0500

    create Skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {Header} from "./layout/header/Header.tsx";
 import {Main} from "./layout/main/Main.tsx";
+import {Skills} from "./layout/skills/Skills.tsx";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Main/>
+            <Skills/>
         </div>
     )
 }
