b7325b2d4ae2be4a67b34f3d5f2ba4a16efd6fdd
Sergei <lyamin.sls@gmail.com>
2025-02-05 21:08:32 +0500

    connect fonts, global styled and add conteiner

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,6 +2,8 @@
 import {Logo} from "../../components/logo/Logo.tsx";
 import styled from "styled-components";
 import {Menu} from "../../components/menu/Menu.tsx";
+import {Container} from "../../components/Container.ts";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 const items = ['Home', 'Skills', 'Works', 'testimony', 'Contact']
 
@@ -9,15 +11,17 @@ const items = ['Home', 'Skills', 'Works', 'testimony', 'Contact']
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items} />
+            <Container>
+                <FlexWrapper justify={"space-between"} align={'center'}>
+                    <Logo/>
+                    <Menu menuItems={items} />
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
     background-color: aqua;
-    display: flex;
-    justify-content: space-between;
 `
 
