f87e01e2365651dcfc4139966cdbe1a03e075220
Sergei <lyamin.sls@gmail.com>
2025-02-06 21:19:11 +0500

    styled Main

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ import {Icon} from "../icon/Icon.tsx";
 export const Logo = () => {
     return (
         <a href=''>
-            <Icon iconId={'codeSvg'}/>
+            <Icon iconId={'code'}/>
         </a>
     );
 };
--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -2,28 +2,52 @@
 import styled from "styled-components";
 import photo from '../../../assets/img/photo.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {Container} from "../../../components/Container.ts";
+import {theme} from "../../../style/Theme.ts";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer.</MainTitle>
-                </div>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-between'}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am <span>Svetlana Dyablo</span></Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
 
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+                    <PhotoWrapper>
+                        <Photo src={photo} alt=""/>
+                    </PhotoWrapper>
+
+                </FlexWrapper>
+            </Container>
+
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
-min-height: 100vh;
-    background-color: darkgrey;
+    min-height: 100vh;
+    background-color: #fff5e7;
+    display: flex;
 `
-
+const PhotoWrapper = styled.div`
+  position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+        
+        position: absolute;
+        z-index: -1;
+        top: -24px;
+        left: 24px;
+    }
+`
 
 const Photo = styled.img`
     width: 350px;
@@ -32,8 +56,34 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-color:violet;
+    font-weight: 400;
+    font-size: 27px;
 `
 const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-weight: 700;
+    font-size: 50px;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+    
+    span {
+        position: relative;
+        z-index: 0;
+        &::before {
+            content: ' ';
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent};
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
+`
 
+const SmallText = styled.h2`
+    font-weight: 400;
+    font-size: 14px;
 `
