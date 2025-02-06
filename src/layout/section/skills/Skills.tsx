c38905621d318b90cea422054b1aad2d490848fe
Sergei <lyamin.sls@gmail.com>
2025-02-06 22:41:51 +0500

    styled Skill

--- src/layout/section/skills/Skills.tsx
+++ src/layout/section/skills/Skills.tsx
@@ -1,39 +1,40 @@
 // import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
-// import {Icon} from "../../../components/icon/Icon.tsx";
 import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Skill} from "./skill/Skill.tsx";
+import {Container} from "../../../components/Container.ts";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>My Skills</SectionTitle>
-            <FlexWrapper wrap="wrap" justify="space-between">
-               <Skill iconId={'code'}
-                      title={'html5'}
-                      description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-               <Skill iconId={'css'}
-                      title={'css3'}
-                      description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-               <Skill iconId={'react'}
-                      title={'React'}
-                      description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-                <Skill iconId={'typescript'}
-                       title={'typeScript'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-                <Skill iconId={'style'}
-                       title={'styled components'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-                <Skill iconId={'figma'}
-                       title={'WEB DESIgN'}
-                       description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My Skills</SectionTitle>
+                <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                    <Skill iconId={'code'}
+                           title={'html5'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                    <Skill iconId={'css'}
+                           title={'css3'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                    <Skill iconId={'react'}
+                           title={'React'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                    <Skill iconId={'typescript'}
+                           title={'typeScript'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                    <Skill iconId={'style'}
+                           title={'styled components'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                    <Skill iconId={'figma'}
+                           title={'WEB DESIgN'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim'}></Skill>
+                </FlexWrapper>
+            </Container>
+
         </StyledSkills>
     );
 };
 
 const StyledSkills = styled.section`
-    background-color: #ffe5e4;
-    min-height: 100vh;
 `
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import {GlobalStyled} from "./style/Global.styled.ts";
 
