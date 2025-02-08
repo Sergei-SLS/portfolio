1c67fe1148e0e0c337153541ec11707826175fab
Sergei <lyamin.sls@gmail.com>
2025-02-08 19:59:00 +0500

    stylized Testimony and Contacts

--- src/layout/section/testimony/Testimony.tsx
+++ src/layout/section/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import {SectionTitle} from "../../../components/SectionTitle.tsx";
 import {Icon} from "../../../components/icon/Icon.tsx";
 import {Slider} from "../../../components/slider/Slider.tsx";
 import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
+import {IconWrapper} from "../skills/skill/Skill.tsx";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <FlexWrapper direction={"column"} align={'center'}>
-                <Icon iconId={'quote'}/>
+                <IconWrapper>
+                    <Icon iconId={'quote'}/>
+                </IconWrapper>
                 <Slider></Slider>
             </FlexWrapper>
         </StyledTestimony>
@@ -20,4 +23,8 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
     background-color: blueviolet;
     min-height: 50vh;
+    
+    ${IconWrapper} {
+        margin: 40px 0 72px;
+    }
 `
